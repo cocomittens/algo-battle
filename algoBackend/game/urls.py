4b51788a4a84cea2fdeead369acee3e29d5a09b2
Corrie Stoddard <corriestoddard@gmail.com>
2024-02-26 22:58:22 -0800

    Update urls

--- algoBackend/game/urls.py
+++ algoBackend/game/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('api/game/', views.GameList.as_view()),
-    path('submit_solution/', views.SubmitSolutionView.as_view(), name='submit_solution'),
+    path('api/game/submit/', views.SubmitSolutionView.as_view(), name='submit_solution'),
 ]
