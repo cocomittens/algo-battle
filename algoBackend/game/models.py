5ce54224fa9f0dd42e26c22562917858f23cf3d0
Corrie Stoddard <corriestoddard@gmail.com>
2024-02-26 20:47:16 -0800

    Update game

--- algoBackend/game/models.py
+++ algoBackend/game/models.py
@@ -8,6 +8,9 @@ class Game(models.Model):
     session = models.TextField(max_length=500, blank=True)
     user = models.TextField(max_length=60, blank=True)
     opponent = models.TextField(max_length=60, blank=True)
+    score = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return str(self.id)
