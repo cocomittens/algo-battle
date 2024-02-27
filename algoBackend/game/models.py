535f0acca7bb1f4f85ffe7932fc9b411772fdc52
Corrie Stoddard <corriestoddard@gmail.com>
2024-02-26 19:50:44 -0800

    Migrate game

--- algoBackend/game/models.py
+++ algoBackend/game/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    id = models.AutoField(primary_key=True)
+    session = models.TextField(max_length=500, blank=True)
+    user = models.TextField(max_length=60, blank=True)
+    opponent = models.TextField(max_length=60, blank=True)
+
+    def __str__(self):
+        return self.id
