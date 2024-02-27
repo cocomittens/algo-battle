af9e2a370592954c4904052bd9e53dc5cc6b7283
Corrie Stoddard <corriestoddard@gmail.com>
2024-02-26 19:38:20 -0800

    Add GET

--- algo-frontend/app/api/game/route.js
+++ algo-frontend/app/api/game/route.js
@@ -1,4 +1,12 @@
 import { NextResponse } from "next/server";
+export async function GET(request) {
+  console.log(request.body);
+  return NextResponse.json(
+    { message: "Data received successfully" },
+    { status: 200 }
+  );
+}
+
 export async function POST(request) {
   console.log(request.body);
   return NextResponse.json(
