4bcb8121bac4c334606028dec9e6d63724bc5b25
Corrie Stoddard <corriestoddard@gmail.com>
2024-02-26 19:27:40 -0800

    revert

--- algo-frontend/app/api/game.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "POST") {
-    console.log(req.body);
-    res.status(200).json({ message: "Data received successfully" });
-  } else {
-    res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
-  }
-}
--- algo-frontend/app/pages/game.tsx
+++ algo-frontend/app/pages/game.tsx
@@ -1,10 +1,7 @@
 "use client";
-import { useRouter } from "next/navigation";
 import NavBar from "../components/navbar";
 
 export default function Game() {
-  const router = useRouter();
-
   return (
     <div>
       <NavBar></NavBar>
