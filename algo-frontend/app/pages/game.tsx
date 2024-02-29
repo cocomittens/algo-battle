afa52fe97c0dbbb58aa033423b77e3df0161214b
Corrie Stoddard <corriestoddard@gmail.com>
2024-02-28 21:49:42 -0800

    Layouts

--- algo-frontend/app/pages/game.tsx
+++ algo-frontend/app/pages/game.tsx
@@ -5,6 +5,7 @@ import NavBar from "../components/navbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
+import { Box } from "@mui/material";
 import { Input } from "@mui/base/Input";
 import CodeMirror from "@uiw/react-codemirror";
 
@@ -22,39 +23,56 @@ export default function Game() {
       <NavBar isGame={true} gameData={submission} />
       <Grid container>
         <Grid container item xs={6}>
-          <Typography variant="h2" textAlign="center">
-            Problem
-          </Typography>
-          <Typography variant="body1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-            efficitur laoreet nibh non tincidunt. Donec sollicitudin finibus
-            velit, sed fermentum metus. Proin fermentum ultrices leo, id
-            suscipit dui tempus vel. Nulla a justo velit. Cras posuere porta
-            orci, eu ultricies ex suscipit at. Nunc aliquet pellentesque varius.
-            Mauris maximus felis ut dolor venenatis commodo. Aliquam sagittis
-            nisl non fermentum placerat. Quisque vitae ex metus. Curabitur
-            auctor eget purus vitae sodales. Aenean at nunc erat. Ut rhoncus vel
-            orci at placerat. Suspendisse tempus varius metus sit amet
-            condimentum. Pellentesque pretium turpis eu nisi aliquet, vitae
-            facilisis nulla pulvinar.
-          </Typography>
-          <Grid container item>
-            <Grid item xs={12}>
+          <Grid container item height="60vh" overflow="scroll">
+            <Typography variant="h2" textAlign="center">
+              Problem
+            </Typography>
+            <Typography variant="body1">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              efficitur laoreet nibh non tincidunt. Donec sollicitudin finibus
+              velit, sed fermentum metus. Proin fermentum ultrices leo, id
+              suscipit dui tempus vel. Nulla a justo velit. Cras posuere porta
+              orci, eu ultricies ex suscipit at. Nunc aliquet pellentesque
+              varius. Mauris maximus felis ut dolor venenatis commodo. Aliquam
+              sagittis nisl non fermentum placerat. Quisque vitae ex metus.
+              Curabitur auctor eget purus vitae sodales. Aenean at nunc erat. Ut
+              rhoncus vel orci at placerat. Suspendisse tempus varius metus sit
+              amet condimentum. Pellentesque pretium turpis eu nisi aliquet,
+              vitae facilisis nulla pulvinar. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. In efficitur laoreet nibh non
+              tincidunt. Donec sollicitudin finibus velit, sed fermentum metus.
+              Proin fermentum ultrices leo, id suscipit dui tempus vel. Nulla a
+              justo velit. Cras posuere porta orci, eu ultricies ex suscipit at.
+              Nunc aliquet pellentesque varius. Mauris maximus felis ut dolor
+              venenatis commodo. Aliquam sagittis nisl non fermentum placerat.
+              Quisque vitae ex metus. Curabitur auctor eget purus vitae sodales.
+              Aenean at nunc erat. Ut rhoncus vel orci at placerat. Suspendisse
+              tempus varius metus sit amet condimentum. Pellentesque pretium
+              turpis eu nisi aliquet, vitae facilisis nulla pulvinar.
+            </Typography>
+          </Grid>
+          <Grid container item height="30vh" overflow="scroll">
+            <Grid item xs={12} bgcolor="#333">
               <Typography>Cat: yooo</Typography>
             </Grid>
             <Grid item xs={12}>
-              <Input placeholder="Type your code here" />
+              <Input placeholder="Type your message here..." />
             </Grid>
           </Grid>
         </Grid>
-        <Grid container item xs={6}>
+        <Grid container item xs={6} height="30vh">
           <Grid item>
             <CodeMirror value={submission.typed_code} height="20vh" />
           </Grid>
           <Grid item xs={12}>
             <Typography variant="h2">Results</Typography>
-            <Typography variant="body1">Last failed test case:</Typography>
+            <Typography variant="body1">Test case:</Typography>
+            <Typography variant="body1">Output:</Typography>
+            <Typography variant="body1">Expected output:</Typography>
             <Typography variant="body1">Test cases remaining:</Typography>
+            <Typography variant="body1">
+              Opponent test cases remaining:
+            </Typography>
           </Grid>
         </Grid>
       </Grid>
